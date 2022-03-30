6c3e583e848e961339423fd582cccd53be893519
rumesaliaquat <rumesaliaquat@gmail.com>
2022-03-31 00:32:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let obj = {
+        fname:'rumesa',
+        lname:'liaquat',
+        phoneNo :1234
+    }
+ //extracting obj values
+ var arr = Object.values(obj)
+ console.log(arr)
+ //extracting obj keys
+ var arr1 = Object.keys(obj)
+ console.log(arr1)
+ 
+     
